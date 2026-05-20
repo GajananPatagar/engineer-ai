fa862f58a900b5cd6bebce5a177a77ca00698bd3
GajananPatagar <gaju21126@gmail.com>
2026-05-20 14:29:38 +0530

    fixed build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.namer_app"
+
+    namespace = "com.example.engineer_ai"
+
     compileSdk = flutter.compileSdkVersion
+
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -16,25 +18,33 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.namer_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+
+        applicationId = "com.example.engineer_ai"
+
+        minSdk = 24
+
         targetSdk = flutter.targetSdkVersion
+
         versionCode = flutter.versionCode
+
         versionName = flutter.versionName
     }
-release {
-    isMinifyEnabled = false
-    isShrinkResources = false
 
-    signingConfig = signingConfigs.getByName("debug")
-}
+    buildTypes {
+
+        getByName("release") {
+
+            isMinifyEnabled = false
+
+            isShrinkResources = false
+
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
 
 flutter {
